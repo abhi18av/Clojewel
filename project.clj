b8d1ff387f904d2fed4019c23de596f847b4d7ef
Abhinav Sharma <abhi18av@gmail.com>
2017-08-18 18:18:29 +0530

    added the libraries

--- project.clj
+++ project.clj
@@ -4,8 +4,93 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [
+
+                 [org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/clojurescript "1.9.854" :scope "provided"]
+
+                            ;;; EXPERIMENTS
+
+                            [instaparse "1.4.7"]
+
+                            ;[org.bytedeco.javacpp-presets/llvm-platform "4.0.0-1.3"]
+
+
+
+                            ;;; IMPORTANT STUFF
+                            ;; Frozen Clojurescript version
+                            ;[org.clojure/clojurescript "1.9.854"]
+                            ;[adzerk/boot-cljs "2.1.1" :scope "test"]
+
+
+
+                            ;; Frozen clojure version
+                            ;[org.clojure/clojure "1.9.0-alpha17"]
+                            
+                            ;; Testing
+                            ;[adzerk/boot-test "1.2.0" :scope "test"]
+
+
+                            ;; Matrix stuff
+                            [net.mikera/core.matrix "0.60.3"]
+                            [net.mikera/vectorz-clj "0.47.0"]
+
+                            ;; Asynchronous stuff
+                            [org.clojure/core.async "0.3.443"]
+
+                            ;; Pattern matching in clojure
+                            [org.clojure/core.match "0.3.0-alpha5"]
+
+
+
+                            ;; Typed clojure
+                            ;; TODO Creates an issue with cider's nrepl - probably transitive dependency
+                            [org.clojure/core.typed "0.4.0"]
+
+                            ;; TODO Middleware - might help to mitigate the cider error caused by core.typed
+                            ;[org.clojure/tools.nrepl "0.2.12" :scope "test"]
+
+                            ; Quickcheck
+                            [org.clojure/test.check "0.10.0-alpha2"]
+
+                            ;; MiniKanren
+                            [org.clojure/core.logic "0.8.11"]
+
+                            ;; Clojure reader in clojure
+                            [org.clojure/tools.reader "1.0.5"]
+
+                            ;; CLI functionality for clojure
+                            [org.clojure/tools.cli "0.3.5"]
+
+                            ;; Namespace resolution functionality in clojure
+                            [org.clojure/tools.namespace "0.2.11"]
+
+                            ;; Clojure AST to EDn
+                            [org.clojure/tools.analyzer "0.6.9"]
+
+
+                            ;; Combinatorics library
+                            [org.clojure/math.combinatorics "0.1.4"]
+
+                            ;; Dependency resolution
+                            [org.clojure/tools.deps.alpha "0.1.29"]
+
+                            ;; Documentation using marginalia
+                            ;[it.frbracch/boot-marginalia "0.1.3-1"]
+
+                            ;; Boot documenataion tool Codox
+                            ;[boot-codox "0.10.3" :scope "test"]
+
+                            ;; Shell library
+                            [me.raynes/conch "0.8.0"]
+
+
+
+
+
+
+
+
                  [com.cognitect/transit-clj "0.8.300"]
                  [ring "1.6.2"]
                  [ring/ring-defaults "0.3.1"]
