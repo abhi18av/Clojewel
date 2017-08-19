83f3381747ed5a69a18afac8cc63f5fae65326fa
Abhinav Sharma <abhi18av@gmail.com>
2017-08-19 16:44:31 +0530

    shuffled the folder for a better organization within the initial chestnut template

--- src/clj/clojewel/jlrepl/instaparse.clj
+++ src/clj/clojewel/jlrepl/instaparse.clj
@@ -1,22 +1,24 @@
-(ns boot.user
+(ns user
   (:require [instaparse.core :as insta]
-            [clojewel.utils :as utils]))
+            [clojewel.jlrepl.utils :as utils]))
+
+(def base-dir "./src/clj/clojewel/jlrepl/")
+
 
 (defn julia-create-instaparse-set-jlir
   "Convert the scratch file to it's JLIR version and show"
   []
-  (let [driver-file  "./src/clojewel/file_to_jlir.jl"
-        source-file  "./src/clojewel/instaparse_set.jl"
-        output-file  "./src/clojewel/instaparse_set.jlir"]
+  (let [driver-file  (str base-dir "file_to_jlir.jl")
+        source-file  (str base-dir "instaparse_set.jl")
+        output-file  (str base-dir "instaparse_set.jlir")]
     (utils/julia-eval-file-with-driver driver-file source-file output-file)))
-
 
 (julia-create-instaparse-set-jlir)
 
 (defn shell-show-instaparse-set
   "Show the \"normal julia syntax\" contents of scratch.jl file"
   []
-  (let [file "./src/clojewel/instaparse_set.jl"]
+  (let [file (str base-dir "instaparse_set.jl")]
     (utils/shell-show-file-content file)))
 
 (shell-show-instaparse-set)
@@ -24,16 +26,15 @@
 (defn shell-show-instaparse-jlir
   "Show the \"JLIR\" contents of scratch.jlir file"
   []
-  (let [file "./src/clojewel/instaparse_set.jlir"]
+  (let [file (str base-dir "instaparse_set.jlir")]
     (utils/shell-show-file-content file)))
 
 (shell-show-instaparse-jlir)
 
 (defn shell-open-both-instaparse-set-in-subl []
   (do
-    (utils/shell-open-file-in-sublime "./src/clojewel/instaparse_set.jl")
-    (utils/shell-open-file-in-sublime "./src/clojewel/instaparse_set.jlir")))
-
+    (utils/shell-open-file-in-sublime (str base-dir "instaparse_set.jl"))
+    (utils/shell-open-file-in-sublime (str base-dir "instaparse_set.jlir"))))
 
 (shell-open-both-instaparse-set-in-subl)
 
@@ -43,8 +44,6 @@
               <rparen> = <')'>
               number = #'[0-9]+'
               <space> = <#'[ ]*'>"))
-
-(parser "1")
 
 (parser "(1)")
 
